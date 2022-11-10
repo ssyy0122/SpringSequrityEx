12661e7b14cb7e7f990ad6659c782d446c2fefd3
ssyy0122 <z01039118740@gmail.com>
2022-11-10 15:40:27 +0900

    first commit

---
+++ src/main/java/com/example/springsequrity/SpringSequrityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsequrity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSequrityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSequrityApplication.class, args);
+    }
+
+}
