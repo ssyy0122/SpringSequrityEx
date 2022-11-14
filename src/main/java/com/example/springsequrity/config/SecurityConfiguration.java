8f48d0db8e3dec7c01cd28730dbcb4d7391224ea
ssyy0122 <z01039118740@gmail.com>
2022-11-14 22:49:05 +0900

    SecurityConfiguration file add

---
+++ src/main/java/com/example/springsequrity/config/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package com.example.springsequrity.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@EnableWebSecurity
+@AllArgsConstructor
+
+public class SecurityConfiguration {
+
+}
