a200faf053282ee894c400221bcb86b64b242314
ssyy0122 <z01039118740@gmail.com>
2022-11-14 22:49:30 +0900

    Import delete

--- src/main/java/com/example/springsequrity/config/SecurityConfiguration.java
+++ src/main/java/com/example/springsequrity/config/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.example.springsequrity.config;
 
 import lombok.AllArgsConstructor;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @EnableWebSecurity
 @AllArgsConstructor
