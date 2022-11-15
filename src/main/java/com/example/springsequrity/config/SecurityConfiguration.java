e99f6b20ece74ed14ddc7943e6e8d9393ccb7e10
ssyy0122 <z01039118740@gmail.com>
2022-11-15 23:27:22 +0900

    권한설정

--- src/main/java/com/example/springsequrity/config/SecurityConfiguration.java
+++ src/main/java/com/example/springsequrity/config/SecurityConfiguration.java
@@ -2,9 +2,11 @@ package com.example.springsequrity.config;
 
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @AllArgsConstructor
@@ -16,5 +18,14 @@ public class SecurityConfiguration {
     public static BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/signUp").permitAll() //설정한 리소스는 인증 필요없음
+                .anyRequest().authenticated() //그 외 모든 리소스들은 인증필요
+                .and()
 
+        return http.build();
+    }
 }
