5262e29dd4e754571c51985454cc56274b3c6889
ssyy0122 <z01039118740@gmail.com>
2022-11-15 22:59:17 +0900

    암호화를 위해 BCryptPasswordEncoder 빈으로 등록

--- src/main/java/com/example/springsequrity/config/SecurityConfiguration.java
+++ src/main/java/com/example/springsequrity/config/SecurityConfiguration.java
@@ -1,11 +1,20 @@
 package com.example.springsequrity.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @AllArgsConstructor
 
 public class SecurityConfiguration {
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public static BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
