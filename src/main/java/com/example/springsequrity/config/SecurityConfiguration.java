f404062f561b8df20614a2b01d2bb86a0fbe7c0d
ssyy0122 <z01039118740@gmail.com>
2022-11-16 10:23:55 +0900

    Config설정 완료

--- src/main/java/com/example/springsequrity/config/SecurityConfiguration.java
+++ src/main/java/com/example/springsequrity/config/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.example.springsequrity.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,6 +22,7 @@ public class SecurityConfiguration {
     public static BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -40,5 +43,10 @@ public class SecurityConfiguration {
 
 
         return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 }
