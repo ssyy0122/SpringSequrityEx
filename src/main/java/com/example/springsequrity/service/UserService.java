893720b10a201d33fef9a1fde8242ae434b12d20
ssyy0122 <z01039118740@gmail.com>
2022-11-22 23:21:05 +0900

    Service

--- src/main/java/com/example/springsequrity/service/UserService.java
+++ src/main/java/com/example/springsequrity/service/UserService.java
@@ -1,8 +1,18 @@
 package com.example.springsequrity.service;
 
 import com.example.springsequrity.persistence.model.UserDto;
+import com.example.springsequrity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     public void insert(UserDto userDto) {
+        userDto.setPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
+        userRepository.save(userDto.toEntity());
     }
 }
